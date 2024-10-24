e7a7862924be73a5bfe6f781c031eb50e522455c
ningolly <nicollydev@gmail.com>
2024-10-24 00:08:37 -0300

    Adicionando select ao update

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./delete.css">
+    <link rel="stylesheet" type="text/css" href="styles/delete.css">
     <title>Agenda</title>
 </head>
 <body>
--- update.php
+++ update.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./update.css">
+    <link rel="stylesheet" type="text/css" href="styles/update.css">
     <title>Agenda</title>
 </head>
+
 <body>
     <div class="container">
         <div class="menu">
@@ -17,31 +19,51 @@
             </div>
             <hr class="separa">
             <button class="update">
-                <img src="imagens/update.png" width="18px" height="18px"> UPDATE
+                <img src="imagens/update.png" width="18px" height="18px"> Update
             </button>
-    </div>
-    <div class="navBar">
-            <input type="text" class="buscar" placeholder="Procurar"></input>
+        </div>
+        <div class="navBar">
+            <input type="text" class="buscar" placeholder="Procurar">
             <a>Update</a>
-    </div>
-    <div class="banco">
-        <div class="textUpdate">
-            Selecione o evento que deseja atualizar
         </div>
-        <div class="form">
-            <form action="index.php">
-                <label for="cars">Selecione o nome do Evento que deseja atualizar:</label>
-                <select name="cars" id="cars">
-                  <option value="volvo">Volvo</option>
-                  <option value="saab">Saab</option>
-                  <option value="opel">Opel</option>
-                  <option value="audi">Audi</option>
-                </select>
-                <br><br>
-                <input type="submit" value="Submit">
-              </form>
-        </div>
+        <div class="banco">
+            <div class="textUpdate">
+                Selecione o evento que deseja atualizar
+            </div>
+            <div class="form">
+                <form action="index.php" method="POST">
+                    <label for="cars">Selecione o ID do Evento que deseja atualizar:</label>
+                    <select name="cars" id="cars">
+                        <?php
+                        $host = "localhost";
+                        $user = "root";
+                        $pass = "";
+                        $base = "defaultBase"; // Certifique-se de que o nome do banco de dados está correto
+                        $conn = mysqli_connect($host, $user, $pass, $base);
+
+                        if (!$conn) {
+                            die("Conexão falhou: " . mysqli_connect_error());
+                        }
+
+                        $asnw = mysqli_query($conn, "SELECT * FROM evento");
+                        if (!$asnw) {
+                            die("Erro na consulta: " . mysqli_error($conn));
+                        }
+
+                        while ($write = mysqli_fetch_array($asnw)) {
+                            echo "<option value='{$write['id_evento']}'>{$write['id_evento']}</option>"; // Mostrando o ID
+                        }
+
+                        mysqli_close($conn); // Fecha a conexão com o banco de dados
+
+                        ?>
+                    </select>
+                    <br><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
